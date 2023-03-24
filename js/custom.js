48df35679a6b3fd5401c218355a3c9775ee60076
Lakshmi <89020031+Lakshmai@users.noreply.github.com>
2023-03-24 17:57:23 +0530

    js

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+
+// $(window).load(function() {  
+//     $("#loader").fadeOut(1000);  
+// });
+
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement({ pageLanguage: 'en', includedLanguages: "hi,te,ta,kn,ml,en" }, 'google_translate_element');
+}
