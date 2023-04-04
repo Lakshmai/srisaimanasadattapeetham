aea4d998570b63fbf092f9a61eb2a949f464969d
Lakshmi Prasanna Teja Aleti <me@lakshmii.ml>
2023-04-04 12:23:45 +0530

    config paytm

---
+++ config_paytm.php
@@ -0,0 +1,33 @@
+<?php
+/*
+- Use PAYTM_ENVIRONMENT as 'PROD' if you wanted to do transaction in production environment else 'TEST' for doing transaction in testing environment.
+- Change the value of PAYTM_MERCHANT_KEY constant with details received from Paytm.
+- Change the value of PAYTM_MERCHANT_MID constant with details received from Paytm.
+- Change the value of PAYTM_MERCHANT_WEBSITE constant with details received from Paytm.
+- Above details will be different for testing and production environment.
+*/
+
+/*
+define('PAYTM_ENVIRONMENT', 'TEST'); // TEST
+define('PAYTM_MERCHANT_KEY', '8KrHHO_Ivti8%xFK'); //Change this constant's value with Merchant key received from Paytm.
+define('PAYTM_MERCHANT_MID', 'rhFAWo20598959663316'); //Change this constant's value with MID (Merchant ID) received from Paytm.
+define('PAYTM_MERCHANT_WEBSITE', 'DEFAULT'); //Change this constant's value with Website name received from Paytm.
+*/
+
+define('PAYTM_ENVIRONMENT', 'PROD'); // PROD
+define('PAYTM_MERCHANT_KEY', 'G3Ej0A13#3JcshRR'); //Change this constant's value with Merchant key received from Paytm.
+define('PAYTM_MERCHANT_MID', 'mnCwAM20571325043613'); //Change this constant's value with MID (Merchant ID) received from Paytm.
+define('PAYTM_MERCHANT_WEBSITE', 'DEFAULT'); //Change this constant's value with Website name received from Paytm.
+
+$PAYTM_STATUS_QUERY_NEW_URL='https://securegw-stage.paytm.in/order/status';
+$PAYTM_TXN_URL='https://securegw-stage.paytm.in/order/process';
+if (PAYTM_ENVIRONMENT == 'PROD') {
+	$PAYTM_STATUS_QUERY_NEW_URL='https://securegw.paytm.in/merchant-status/getTxnStatus';
+	$PAYTM_TXN_URL='https://securegw.paytm.in/theia/processTransaction';
+}
+
+define('PAYTM_REFUND_URL', '');
+define('PAYTM_STATUS_QUERY_URL', $PAYTM_STATUS_QUERY_NEW_URL);
+define('PAYTM_STATUS_QUERY_NEW_URL', $PAYTM_STATUS_QUERY_NEW_URL);
+define('PAYTM_TXN_URL', $PAYTM_TXN_URL);
+?>
